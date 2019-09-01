2a7ba42ca1f97669a74ff69ef196fbc258a31be9
Heuller Soares Vilela Silva <heullergit@gmail.com>
2019-09-01 15:58:11 -0300

    verificacao de cartao vermelho terminado

--- dados_da_partida.py
+++ dados_da_partida.py
@@ -5,75 +5,35 @@ from Enum.Meses import qualMes
 from funcoes.funcoes import dia_da_semana, identificar_minuto_expulsao_segundo_amarelo, identificar_minuto_expulsao_vermelho_direto
 import datetime
 
-page = requests.get("https://www.academiadasapostasbrasil.com/stats/match/brasil-stats/brasileirao-serie-a/sao-paulo/gremio/2989080/1/live")
+page = requests.get("https://www.academiadasapostasbrasil.com/stats/match/brasil-stats/brasileirao-serie-a/avai/corinthians/2989073/1/live")
 soup = BeautifulSoup(page.content, 'html.parser')
 
-
 estatisticas = soup.find("table", class_="match_stats_center")
-cartao_vermelho = estatisticas.find(class_="red_cards")
-segundo_amarelo = estatisticas.find(class_="yellow_red_cards")
-
-###VERIFICACAO DE CARTÃO DO PRIMEIRO MODO POR MEIO DAS ESTATISTICAS
-if((cartao_vermelho) and (segundo_amarelo)):
-    teve_cartao_vermelho = True
-    print('cartao vermelho e segundo amarelo no mesmo jogo')
-    cartao_vermelho_casa = int(cartao_vermelho.find(class_="stat_value_number_team_A").get_text().strip())
-    cartao_vermelho_visitante = int(cartao_vermelho.find(class_="stat_value_number_team_B").get_text().strip())
-
-    segundo_amarelo_casa = int(segundo_amarelo.find(class_="stat_value_number_team_A").get_text().strip())
-    segundo_amarelo_visitante = int(segundo_amarelo.find(class_="stat_value_number_team_B").get_text().strip())
-
-elif((cartao_vermelho)):
-    teve_cartao_vermelho = True
-    print('cartao vermelho direto')
-    cartao_vermelho_casa = int(cartao_vermelho.find(class_="stat_value_number_team_A").get_text().strip())
-    cartao_vermelho_visitante = int(cartao_vermelho.find(class_="stat_value_number_team_B").get_text().strip())
-elif((segundo_amarelo)):
-    teve_cartao_vermelho = True
-    print('segundo amarelo e expulsao')
-    segundo_amarelo_casa = int(segundo_amarelo.find(class_="stat_value_number_team_A").get_text().strip())
-    segundo_amarelo_visitante = int(segundo_amarelo.find(class_="stat_value_number_team_B").get_text().strip())
-else:
-    teve_cartao_vermelho = False
-    print('nao houve expulsao nesse jogo')
-
-
 
 time_a = soup.find(id="team_A_")
-icone_segundo_amarelo = time_a.find_all(class_="event aa-icon-Y2C")
-icone_vermelho_direto = time_a.find_all(class_="event aa-icon-RC")
-
-
-minutos_seg_amarelo = identificar_minuto_expulsao_segundo_amarelo(icone_segundo_amarelo)
-minutos_ver_direto = identificar_minuto_expulsao_vermelho_direto(icone_vermelho_direto)
-minutos_expulsoes = minutos_seg_amarelo + minutos_ver_direto
-
 time_b = soup.find(id="team_B_")
-icone_segundo_amarelo = time_b.find_all(class_="event aa-icon-Y2C")
-icone_vermelho_direto = time_b.find_all(class_="event aa-icon-RC")
 
-minutos_seg_amarelo = identificar_minuto_expulsao_segundo_amarelo(icone_segundo_amarelo)
-minutos_ver_direto = identificar_minuto_expulsao_vermelho_direto(icone_vermelho_direto)
-minutos_expulsoes += minutos_seg_amarelo + minutos_ver_direto
+minutos_seg_amarelo = identificar_minuto_expulsao_segundo_amarelo(time_a)
+minutos_ver_direto = identificar_minuto_expulsao_vermelho_direto(time_a)
+minutos_expulsoes_time_a = minutos_seg_amarelo + minutos_ver_direto
+
+minutos_seg_amarelo = identificar_minuto_expulsao_segundo_amarelo(time_b)
+minutos_ver_direto = identificar_minuto_expulsao_vermelho_direto(time_b)
+minutos_expulsoes_time_b = minutos_seg_amarelo + minutos_ver_direto
+
+print(minutos_expulsoes_time_a)
+print(minutos_expulsoes_time_b)
+    #minutos_expulsoes = minutos_seg_amarelo + minutos_ver_direto
 
 
-# for seg_amarelo in icone_segundo_amarelo:
-#     seg_amarelo_results_string = str(seg_amarelo).replace('tr&gt;','').replace('th&gt;','').replace('td&gt;','').replace('&lt;','').replace('&gt','').replace('/','').replace(';','').replace('span','').replace('undermark','').replace('overermark','').replace('class=','').replace('\""','').replace('+','').replace('.','')
-#     seg_amarelo_results_string_split = seg_amarelo_results_string.split()
-#     indice = seg_amarelo_results_string_split.index("aa-icon-Y2C'")
-#     minuto = seg_amarelo_results_string_split[indice+4]
-#     minuto = int(minuto.replace("'",''))
-#     minutos_expulsoes.append(minuto)
+    # time_b = soup.find(id="team_B_")
+    # icone_segundo_amarelo = time_b.find_all(class_="event aa-icon-Y2C")
+    # icone_vermelho_direto = time_b.find_all(class_="event aa-icon-RC")
+    # minutos_seg_amarelo = identificar_minuto_expulsao_segundo_amarelo(icone_segundo_amarelo)
+    # minutos_ver_direto = identificar_minuto_expulsao_vermelho_direto(icone_vermelho_direto)
+    # minutos_expulsoes += minutos_seg_amarelo + minutos_ver_direto
 
-# for ver_direto in icone_vermelho_direto:
-#     ver_direto_results_string = str(ver_direto).replace('tr&gt;','').replace('th&gt;','').replace('td&gt;','').replace('&lt;','').replace('&gt','').replace('/','').replace(';','').replace('span','').replace('undermark','').replace('overermark','').replace('class=','').replace('\""','').replace('+','').replace('.','')
-#     ver_direto_results_string_split = ver_direto_results_string.split()
-#     indice = ver_direto_results_string_split.index("aa-icon-RC'")
-#     minuto = ver_direto_results_string_split[indice+4]
-#     minuto = int(minuto.replace("'",''))
-#     minutos_expulsoes.append(minuto)
-
-print(minutos_expulsoes)
+#print(minutos_expulsoes)
 
 
 
--- funcoes/funcoes.py
+++ funcoes/funcoes.py
@@ -18,26 +18,65 @@ def dia_da_semana(ano,mes,dia):
     return dia_da_semana
 
 
-def identificar_minuto_expulsao_segundo_amarelo(icone_segundo_amarelo):
+def identificar_minuto_expulsao_segundo_amarelo(time):
     minutos_expulsoes = []
-    for seg_amarelo in icone_segundo_amarelo:
-        seg_amarelo_results_string = str(seg_amarelo).replace('tr&gt;','').replace('th&gt;','').replace('td&gt;','').replace('&lt;','').replace('&gt','').replace('/','').replace(';','').replace('span','').replace('undermark','').replace('overermark','').replace('class=','').replace('\""','').replace('+','').replace('.','')
-        seg_amarelo_results_string_split = seg_amarelo_results_string.split()
-        indice = seg_amarelo_results_string_split.index("aa-icon-Y2C'")
-        minuto = seg_amarelo_results_string_split[indice+4]
-        minuto = int(minuto.replace("'",''))
-        minutos_expulsoes.append(minuto)
+    seg_amarelo_results_string = str(time).replace('tr&gt;','').replace('th&gt;','').replace('td&gt;','').replace('&lt;','').replace('&gt','').replace('/','').replace(';','').replace('span','').replace('undermark','').replace('overermark','').replace('class=','').replace('\""','').replace('.','')
+    seg_amarelo_results_string_split = seg_amarelo_results_string.split()
+    if "aa-icon-Y2C'" in seg_amarelo_results_string_split:
+        posicao = 0
+        posicoes = []
+        for pos in seg_amarelo_results_string_split:
+            if(pos == "aa-icon-Y2C'"):
+                posicoes.append(posicao + 4)
+            posicao += 1
+
+        for pos in posicoes:
+            minuto = seg_amarelo_results_string_split[pos]
+            minuto = minuto.replace("'",'')
+            if '+' in minuto:
+                minuto = minuto.split('+')
+                minuto = int(minuto[0])+ int(minuto[1])
+            else:
+                minuto = int(minuto)
+            minutos_expulsoes.append(minuto)
+
+        
+        # indice = seg_amarelo_results_string_split.index("aa-icon-Y2C'")
+        # minuto = seg_amarelo_results_string_split[indice+4]
+        # minuto = minuto.replace("'",'')
+        # if '+' in minuto:
+        #     minuto = minuto.split('+')
+        #     minuto = int(minuto[0])+ int(minuto[1])
+        # else:
+        #     minuto = int(minuto)
+        
+        # minutos_expulsoes.append(minuto)
 
     return minutos_expulsoes
 
-def identificar_minuto_expulsao_vermelho_direto(icone_vermelho_direto):
+def identificar_minuto_expulsao_vermelho_direto(time):
     minutos_expulsoes = []
-    for ver_direto in icone_vermelho_direto:
-        ver_direto_results_string = str(ver_direto).replace('tr&gt;','').replace('th&gt;','').replace('td&gt;','').replace('&lt;','').replace('&gt','').replace('/','').replace(';','').replace('span','').replace('undermark','').replace('overermark','').replace('class=','').replace('\""','').replace('+','').replace('.','')
-        ver_direto_results_string_split = ver_direto_results_string.split()
-        indice = ver_direto_results_string_split.index("aa-icon-RC'")
-        minuto = ver_direto_results_string_split[indice+4]
-        minuto = int(minuto.replace("'",''))
-        minutos_expulsoes.append(minuto)
+    ver_direto_results_string = str(time).replace('tr&gt;','').replace('th&gt;','').replace('td&gt;','').replace('&lt;','').replace('&gt','').replace('/','').replace(';','').replace('span','').replace('undermark','').replace('overermark','').replace('class=','').replace('\""','').replace('.','')
+    ver_direto_results_string_split = ver_direto_results_string.split()
+
+    ###VERIFICAR MAIS DE UMA OCORRENCIAS
+    
+    if "aa-icon-RC'" in ver_direto_results_string_split:
+        posicao = 0
+        posicoes = []
+        for pos in ver_direto_results_string_split:
+            if(pos == "aa-icon-RC'"):
+                posicoes.append(posicao + 4)
+            posicao += 1
+
+        for pos in posicoes:
+            minuto = ver_direto_results_string_split[pos]
+            minuto = minuto.replace("'",'')
+            if '+' in minuto:
+                minuto = minuto.split('+')
+                minuto = int(minuto[0])+ int(minuto[1])
+            else:
+                minuto = int(minuto)
+            minutos_expulsoes.append(minuto)
 
     return minutos_expulsoes
